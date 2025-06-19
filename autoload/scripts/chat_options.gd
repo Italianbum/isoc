aee694e7f50ea4c88ad968fd50924d4a213f142d
Italianbum <iBumDev42@gmail.com>
2025-06-19 15:02:20 -0400

    voice lines import

---
+++ autoload/scripts/chat_options.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var chat_options: Array[String] = [
+	"How are you feeling?",
+	"Does anything hurt?",
+	"How can I help?",
+	"Have you taken your medications?",
+	"How long have you been here?",
+	"How long have you been sick?",
+	"Do you feel any strong pain?",
+	"Do you have any issues with your senses? ",
+	"Let me check your heart rate.",
+	"Let me check your eyesight.",
+	"Let me check your blood pressure.",
+	"Let me check your breathing.",
+	"Can you open your mouth and say aaaaaaah.",
+	"Can you stand up and hold up your arms for me?",
+	"Let's test your reflexes.",
+	"Are you happy here?",
+	"Would you like to leave?",
+	"Do you trust me?",
+	"Are you worried about anything?"
+	]
